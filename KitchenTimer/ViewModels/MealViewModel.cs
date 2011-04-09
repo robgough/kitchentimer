68538b1f62d973ea7055947f9bff5cfc43632ec5
Rob Gough <robertgough@gmail.com>
2011-04-09 13:49:31 +0100

    main page, and start of view models

---
+++ KitchenTimer/ViewModels/MealViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace KitchenTimer.ViewModels
+{
+    public class MealViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<MealItemViewModel> Timers { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
