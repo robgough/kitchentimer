ef6f7c22869348d9b66f72d77b2744efe645ed61
unknown <Dave@.(none)>
2011-04-09 11:41:28 +0100

    various

---
+++ Model/Meal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class Meal
+    {
+        public int Id;
+
+        public string Name;
+
+        public DateTime LastCooked;
+
+        public bool Favourite;
+
+        public List<Timer> Timers;
+    }
+}
---
+++ Model/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class Timer
+    {
+        public int Id;
+
+        public string Name;
+
+        public TimeSpan CookingTime;
+
+        public TimeSpan StandingTime;
+
+        public string Notes;
+
+        public bool Saved;
+
+        public List<Meal> Meals;
+    }
+}
